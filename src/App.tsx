db5b7e6b9597a0035877359a85ae14a0407db232
Alexnander Nasokin <alexnas2python@gmail.com>
2023-01-24 15:26:02 +0300

    Install and tune tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>Hello Everybody!</h1>
+		<div className='container mx-auto max-w-2xl pt-5'>
+  		<h1>Hello Everybody!</h1>
+			<div>Start me</div>
+		</div>
   );
 }
 
