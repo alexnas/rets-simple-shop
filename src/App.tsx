ab1c3da6d11225bda2c1a86009fbc2f48a255a00
Alexnander Nasokin <alexnas2python@gmail.com>
2023-01-24 19:38:45 +0300

    Restructure Loader, ErrorMessage and hook useProducts from overall component

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import ErrorMessage from './components/ErrorMessage';
+import Loader from './components/Loader';
 import Product from './components/Product'
-import axios from 'axios'
-import { IProduct } from './models';
+import { useProducts } from './hooks/products'; 
+
 
 function App() {
-	const [products, setProducts] = useState<IProduct[]>([])
-	const [loading, setLoading] = useState(false)
-	const [error, setError] = useState('')
-
-	async function fetchProducts() {
-		try {
-			setError('')
-			setLoading(true)
-			const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-			setProducts(response.data)
-			setLoading(false)
-		} catch (err) {
-			setLoading(false)
-			if (axios.isAxiosError(err) && err.message) {
-				setError(err.message)
-			} else {
-				setError('Network Error')
-			}
-		}
-	} 
-
-	useEffect(() => {
-		fetchProducts()
-	},[])
+	const {products, loading, error} = useProducts()
 
   return (
 		<>
 			<div className='container mx-auto max-w-2xl pt-5'>
-			{loading && <div className='text-center'>Loading in progress...</div>}
-			{error && <div className='text-center text-red-600'>{error}</div>}
-				{products.map(product => <Product product={product} key={product.id}/>)}
+			{ loading && <Loader/> }
+			{ error && <ErrorMessage error={error}/> }
+			{ products.map(product => <Product product={product} key={product.id}/>) }
 			</div>
 		</>
   );
---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Loader = (props: Props) => {
+	return (
+		<div className='text-center'>Loading in progress...</div>
+	)
+}
+
+export default Loader
